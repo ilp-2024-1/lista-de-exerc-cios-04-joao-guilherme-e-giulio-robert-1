a301012979c1d4d39e2b7fd9363d681d4b7a6648
RobertMN076 <robert.giulio@escolar.ifrn.edu.br>
2024-08-15 10:01:52 -0300

    questão 04, 06 e alguma parte da 08

--- exercicios.py
+++ exercicios.py
@@ -40,72 +40,113 @@
 #     print('Soma listaimpar2 > Soma listaimpar1')
 
 # Questão 04
-tamanho_da_lista = int(input('Digite o valor das listas: '))
-primeira_lista = []
-primeiro_valor_boolean = False
-primeiro_valor_numerico = 0
-elemento_desejado_cert = ['boolean', 'string', 'numero']
+# tamanho_da_lista = int(input('Digite o valor das listas: '))
+# primeira_lista = []
+# primeiro_valor_boolean = False
+# primeiro_valor_numerico = 0
+# elemento_desejado_cert = ['boolean', 'string', 'numero']
 
-for i in range(tamanho_da_lista):
-    if i == 0:
-        primeiro_valor = input('Digite o primeiro valor string: ')
-        primeira_lista += [primeiro_valor]
-    elif i == 1:
-        primeiro_valor_boolean = bool(input('Digite o primeiro valor boolean: '))
-        primeira_lista += [primeiro_valor_boolean]
-    elif i == 2:
-        primeiro_valor_numerico = int(input('Digite o primeiro valor numerico: '))
-        primeira_lista += [primeiro_valor_numerico]
-    else:
-        outro_valor_numerico = 0
-        elemento_desejado = input('Digite o tipo de elemento desejado: ')
-        while elemento_desejado not in elemento_desejado_cert:
-            print('Elemento inválido.')
-            elemento_desejado = input('Digite outro tipo de elemento desejado: ')
-        if elemento_desejado == elemento_desejado_cert[0]:
-            outro_valor_boolean = bool(input('Digite outro valor boolean: '))
-            primeira_lista += [outro_valor_boolean]
-        elif elemento_desejado == elemento_desejado_cert[1]:
-            outro_valor_string = input('Digite outro valor string: ')
-            primeira_lista += [outro_valor_string]
-        else:
-            outro_valor_numerico = int(input('Digite outro valor numerico: '))
-            primeira_lista += [outro_valor_numerico]
+# for i in range(tamanho_da_lista):
+#     if i == 0:
+#         primeiro_valor = input('Digite o primeiro valor string: ')
+#         primeira_lista += [primeiro_valor]
+#     elif i == 1:
+#         primeiro_valor_boolean = bool(input('Digite o primeiro valor boolean: '))
+#         primeira_lista += [primeiro_valor_boolean]
+#     elif i == 2:
+#         primeiro_valor_numerico = int(input('Digite o primeiro valor numerico: '))
+#         primeira_lista += [primeiro_valor_numerico]
+#     else:
+#         outro_valor_numerico = 0
+#         elemento_desejado = input('Digite o tipo de elemento desejado: ')
+#         while elemento_desejado not in elemento_desejado_cert:
+#             print('Elemento inválido.')
+#             elemento_desejado = input('Digite outro tipo de elemento desejado: ')
+#         if elemento_desejado == elemento_desejado_cert[0]:
+#             outro_valor_boolean = bool(input('Digite outro valor boolean: '))
+#             primeira_lista += [outro_valor_boolean]
+#         elif elemento_desejado == elemento_desejado_cert[1]:
+#             outro_valor_string = input('Digite outro valor string: ')
+#             primeira_lista += [outro_valor_string]
+#         else:
+#             outro_valor_numerico = int(input('Digite outro valor numerico: '))
+#             primeira_lista += [outro_valor_numerico]
 
-segunda_lista = []
-segundo_valor_boolean = False
-segundo_valor_numerico = 0
+# segunda_lista = []
+# segundo_valor_boolean = False
+# segundo_valor_numerico = 0
 
 
-for k in range(tamanho_da_lista):
-    if k == 0:
-        segundo_valor = input('Digite o primeiro valor string: ')
-        segunda_lista += [segundo_valor]
-    elif k == 1:
-        segundo_valor_boolean = bool(input('Digite o primeiro valor boolean: '))
-        segunda_lista += [segundo_valor_boolean]
-    elif k == 2:
-        segundo_valor_numerico = int(input('Digite o primeiro valor numerico: '))
-        segunda_lista += [segundo_valor_numerico]
-    else:
-        segundo_outro_valor_numerico = 0
-        elemento_desejado = input('Digite o tipo de elemento desejado: ')
-        while elemento_desejado not in elemento_desejado_cert:
-            print('Elemento inválido.')
-            elemento_desejado = input('Digite outro tipo de elemento desejado: ')
-        if elemento_desejado == elemento_desejado_cert[0]:
-            segundo_outro_valor_boolean = bool(input('Digite outro valor boolean: '))
-            segunda_lista += [segundo_outro_valor_boolean]
-        elif elemento_desejado == elemento_desejado_cert[1]:
-            segundo_outro_valor_string = input('Digite outro valor string: ')
-            segunda_lista += [segundo_outro_valor_string]
-        else:
-            segundo_outro_valor_numerico = int(input('Digite outro valor numerico: '))
-            segunda_lista += [segundo_outro_valor_numerico]
+# for k in range(tamanho_da_lista):
+#     if k == 0:
+#         segundo_valor = input('Digite o primeiro valor string: ')
+#         segunda_lista += [segundo_valor]
+#     elif k == 1:
+#         segundo_valor_boolean = bool(input('Digite o primeiro valor boolean: '))
+#         segunda_lista += [segundo_valor_boolean]
+#     elif k == 2:
+#         segundo_valor_numerico = int(input('Digite o primeiro valor numerico: '))
+#         segunda_lista += [segundo_valor_numerico]
+#     else:
+#         segundo_outro_valor_numerico = 0
+#         elemento_desejado = input('Digite o tipo de elemento desejado: ')
+#         while elemento_desejado not in elemento_desejado_cert:
+#             print('Elemento inválido.')
+#             elemento_desejado = input('Digite outro tipo de elemento desejado: ')
+#         if elemento_desejado == elemento_desejado_cert[0]:
+#             segundo_outro_valor_boolean = bool(input('Digite outro valor boolean: '))
+#             segunda_lista += [segundo_outro_valor_boolean]
+#         elif elemento_desejado == elemento_desejado_cert[1]:
+#             segundo_outro_valor_string = input('Digite outro valor string: ')
+#             segunda_lista += [segundo_outro_valor_string]
+#         else:
+#             segundo_outro_valor_numerico = int(input('Digite outro valor numerico: '))
+#             segunda_lista += [segundo_outro_valor_numerico]
 
-lista_resultante = []
-total_de_elementos = len(primeira_lista) + len(segunda_lista)
-for n in range(total_de_elementos):
-    lista_resultante += [primeira_lista[n]]
-    lista_resultante += [segunda_lista[n]]
-print(lista_resultante)
+# lista_resultante = []
+# for n in range(tamanho_da_lista):
+#     lista_resultante += [primeira_lista[n]]
+#     lista_resultante += [segunda_lista[n]]
+# print(lista_resultante)
+
+#Questão 06
+
+# tamanho_da_lista = int(input('Digite o tamanho da lista: '))
+
+# lista_um = []
+# for i in range(tamanho_da_lista):
+#     valor_num = int(input('Digite os valores numéricos dentro da lista: '))
+#     lista_um += [valor_num]
+
+# lista_dois = []
+# for j in range(tamanho_da_lista):
+#     valor_string = input('Digite a quantidade de letras proporcional à quantidade de números: ')
+#     lista_dois += [valor_string]
+
+# lista_resultante = []
+# for k in range(tamanho_da_lista):
+#     print(k)
+#     if k % 2 != 0:
+#         lista_resultante += [lista_dois[k]]
+#     else:
+#         lista_resultante += [lista_um[k]]
+# print(lista_resultante)
+
+#Questão 08
+
+lista_inicial = input('Digite os números por espaço: ')
+lista_final = lista_inicial.split()
+
+lista_numero = [int(numero) for numero in lista_final]
+soma_num = 0
+i = 0
+
+for valor in lista_numero:
+    i += 1
+    if valor % 2 != 0:
+        soma_num += valor
+        print(valor, end='+')
+        
+
+
+
