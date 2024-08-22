70d71198deeaed28577ba1cb62964807a36e0094
JG1511 <joaotsi2024@gmail>
2024-08-22 18:16:59 -0300

    questao11

--- exercicios.py
+++ exercicios.py
@@ -255,5 +255,22 @@ for valor in lista_numero:
 # print(f"Números impares: {soma_impar}")
         
 
+#Questão 11
 
+# linhas = int(input("Digite o número de Linhas: "))
+# colunas = int(input("Digite o número de Colunas: "))
+
+# matriz = []
+# for i in range(linhas):
+#     linha = []
+#     for j in range(colunas):
+#         valor = int(input(f"Digite os valores da posição: ({i},{j}): "))
+#         linha += [valor]
+#     matriz += [linha]
+
+# for i in range(linhas):
+#     soma_linha = 0
+#     for j in range(colunas):
+#         soma_linha += matriz[i][j] 
+#     print(f"{matriz[i]} = {soma_linha}")
 
