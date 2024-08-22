00f16cb72befd120c408950c6e53f7a0a561b7e4
JG1511 <joaotsi2024@gmail>
2024-08-22 18:16:11 -0300

    questao10

--- exercicios.py
+++ exercicios.py
@@ -227,6 +227,32 @@ for valor in lista_numero:
     if valor % 2 != 0:
         soma_num += valor
         print(valor, end='+')
+
+#Questão 10
+
+# linhas = 3
+# colunas = 3 
+# matriz = []
+# soma_impar = 0
+
+# for i in range(linhas):
+#     linha = []
+#     for j in range(colunas):
+#         valor = int(input(f"Digite os valores das posicao: ({i},{j}): "))
+#         linha += [valor]
+#     matriz += [linha]
+
+# for i in range(linhas):
+#     for j in range(colunas):
+#         if matriz[i][j] % 2 != 0:
+#             soma_impar += 1
+
+# print("Matriz:")
+
+# for _ in matriz:
+#     print(_)
+
+# print(f"Números impares: {soma_impar}")
         
 
 
