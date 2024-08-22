47e45cb7c53ae2c26de8652f366b8d4f7b7333ba
JG1511 <joaotsi2024@gmail>
2024-08-22 18:13:21 -0300

    questao05

--- exercicios.py
+++ exercicios.py
@@ -137,6 +137,33 @@
 #     lista_resultante += [segunda_lista[n]]
 # print(lista_resultante)
 
+#Questão 05
+
+# tamanho = int(input("Digite quantos valores serao fornecidos: "))
+# lista_usuario = []
+# soma = 0
+
+# for _ in range(tamanho):
+#     valor = int(input("Digite os valores da lista: "))
+#     lista_usuario += [valor]
+
+# menor_valor = lista_usuario[0]
+# maior_valor = lista_usuario [0]
+
+# for elemento in lista_usuario:
+#     if elemento < menor_valor:
+#         menor_valor = elemento
+#     elif elemento > maior_valor:
+#         maior_valor = elemento
+#     soma += elemento
+
+# media = soma / len(lista_usuario)
+
+# print(f"menor valor: {menor_valor}")
+# print(f"maior valor: {maior_valor}")
+# print(f"Media aritimetica: {media}")
+
+
 #Questão 06
 
 # tamanho_da_lista = int(input('Digite o tamanho da lista: '))
