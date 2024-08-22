2b76bab633b05fb90d914a8fe3f686a848a640e3
JG1511 <joaotsi2024@gmail>
2024-08-22 18:19:25 -0300

    questao15

--- exercicios.py
+++ exercicios.py
@@ -318,3 +318,39 @@ for valor in lista_numero:
 # for _ in matriz_resultante:
 #     print(_)
 
+#questão 15
+
+# import random
+
+# matriz = []
+# linhas = random.randint(2,10)
+# colunas = random.randint(2,10)
+
+# for i in range(linhas):
+#     linha = []
+#     for j in range(colunas):
+#         valor = random.randint(100,999)
+#         linha.append(valor)
+#     matriz.append(linha)
+
+# menor_valor = matriz[0][0]
+# maior_valor = matriz[0][0]
+# menor_posicao = (0,0)
+# maior_posicao = (0,0)
+
+# for i in range(linhas):
+#     for j in range(colunas):
+#         if matriz[i][j] < menor_valor:
+#             menor_valor = matriz[i][j]
+#             menor_posicao = (i,j)
+#         if matriz[i][j] > maior_valor:
+#             maior_valor = matriz [i][j]
+#             maior_posicao = (i,j)
+            
+# print("Matriz")
+# for _ in matriz:
+#     print(_)
+
+# print(f"Menor valor: {menor_valor} ({menor_posicao})")
+# print(f"Maior valor: {maior_valor} ({maior_posicao})")
+
